e83b6639f31028fc1e738e5c819240c179d35749
kakazul <93196709+kakazul@users.noreply.github.com>
2021-12-17 19:26:47 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
     <div>
       <a href="/.auth/me">My Info</a>
     </div>
+    <div>
+      <a href="https://b2adaldesktopapps.b2clogin.com/b2adaldesktopapps.onmicrosoft.com/B2C_1_susi/oauth2/v2.0/logout?post_logout_redirect_uri=https://lively-flower-07afe6200.azurestaticapps.net/.auth/logout">Logout</a>
+    </div>
   </div>
 </template>
 
